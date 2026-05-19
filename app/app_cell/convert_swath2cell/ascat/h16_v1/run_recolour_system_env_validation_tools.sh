5bee02c386bca934acc7bf84da6ff9a0dd22f3d8
Fabio Delogu <fabio.delogu@cimafoundation.org>
2026-05-19 13:30:53 +0200

    chore(package): remove old file and not used files

--- app/app_cell/convert_swath2cell/ascat/h16_v1/run_recolour_system_env_validation_tools.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ENV_FOLDER="/root/envs/env_validation_tools"
-
-source "${ENV_FOLDER}/bin/activate"
-
-echo "Validation HSAF environment loaded"
-echo "Python: $(which python)"
-echo "Pip:    $(which pip)"
